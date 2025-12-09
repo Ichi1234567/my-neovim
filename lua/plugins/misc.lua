cccdc4f4c5fdf44ad9994233d82ef6cc00a0206d
ichi <ichi1234567@gmail.com>
2025-12-10 00:07:28 +0800

    modify autopairs

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -5,7 +5,14 @@
 return {
   {
     'windwp/nvim-autopairs',
-    config = true,
+    config = function()
+      require('nvim-autopairs').setup{
+        check_ts = true,        -- 支援 treesitter 判斷
+        enable_moveright = true, -- 光標在閉括號前按空格會跳到右邊
+        map_cr = true,          -- Enter 自動補全
+        map_bs = true,          -- Backspace 自動刪除成對
+      }
+    end,
   },
 
   {
@@ -30,16 +37,16 @@ return {
     config = function()
       -- HAML filetype 專用 switch definitions
       vim.api.nvim_create_autocmd('FileType', {
-        pattern = 'haml',
-        callback = function()
-          vim.b.switch_definitions = {
-            vim.g.switch_builtins.ruby_hash_style,
-            vim.g.switch_builtins.ruby_string,
-            vim.g.switch_builtins.true_false,
-            vim.g.switch_builtins.true_false,
-          }
-        end,
-      })
+          pattern = 'haml',
+          callback = function()
+            vim.b.switch_definitions = {
+              vim.g.switch_builtins.ruby_hash_style,
+              vim.g.switch_builtins.ruby_string,
+              vim.g.switch_builtins.true_false,
+              vim.g.switch_builtins.true_false,
+            }
+          end,
+        })
     end,
   },
 
@@ -60,33 +67,33 @@ return {
       { 'af', function() vim.cmd('normal! vaf') end, mode = 'n', desc = 'Select Around Function' },
       { 'if', function() vim.cmd('normal! vif') end, mode = 'n', desc = 'Select Inner Function' },
 
-      -- 選擇 class
-      { 'ac', function() vim.cmd('normal! vac') end, mode = 'n', desc = 'Select Around Class' },
-      { 'ic', function() vim.cmd('normal! vic') end, mode = 'n', desc = 'Select Inner Class' },
+        -- 選擇 class
+        { 'ac', function() vim.cmd('normal! vac') end, mode = 'n', desc = 'Select Around Class' },
+        { 'ic', function() vim.cmd('normal! vic') end, mode = 'n', desc = 'Select Inner Class' },
 
-      -- 選擇 tag/block
-      { 'at', function() vim.cmd('normal! vat') end, mode = 'n', desc = 'Select Around Tag' },
-      { 'it', function() vim.cmd('normal! vit') end, mode = 'n', desc = 'Select Inner Tag' },
-    },
-    config = function()
-      require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'html', 'javascript', 'typescript', 'tsx', 'css', 'lua' },
-        highlight = { enable = true },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
-              ['at'] = '@block.outer',
-              ['it'] = '@block.inner',
-            },
-          },
-        },
-      }
-    end
-  }
-}
+        -- 選擇 tag/block
+        { 'at', function() vim.cmd('normal! vat') end, mode = 'n', desc = 'Select Around Tag' },
+        { 'it', function() vim.cmd('normal! vit') end, mode = 'n', desc = 'Select Inner Tag' },
+      },
+      config = function()
+        require('nvim-treesitter.configs').setup {
+          ensure_installed = { 'html', 'javascript', 'typescript', 'tsx', 'css', 'lua' },
+          highlight = { enable = true },
+          textobjects = {
+            select = {
+              enable = true,
+              lookahead = true,
+              keymaps = {
+                ['af'] = '@function.outer',
+                  ['if'] = '@function.inner',
+                      ['ac'] = '@class.outer',
+                      ['ic'] = '@class.inner',
+                      ['at'] = '@block.outer',
+                      ['it'] = '@block.inner',
+                    },
+                  },
+                },
+              }
+            end
+          }
+        }
