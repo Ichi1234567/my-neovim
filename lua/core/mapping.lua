ae3cf24a6f3bd51dc995c03e9cd3bf814f8bb680
ichi <ichi1234567@gmail.com>
2026-01-01 15:26:23 +0800

    feat(indent): run visual shift/unshif repeatly

--- lua/core/mapping.lua
+++ lua/core/mapping.lua
@@ -9,3 +9,9 @@ vim.g.mapleader = ','
 -- Visual mode copy
 -- ===================================================
 map('v', '<C-y>', '"+y', { desc = "Copy to clipboard" })
+
+-- ===================================================
+-- Visual mode indentation (keep selection)
+-- ===================================================
+map('v', '<', '<gv', { desc = 'Indent left and reselect' })
+map('v', '>', '>gv', { desc = 'Indent right and reselect' })
